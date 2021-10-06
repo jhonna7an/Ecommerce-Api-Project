278b823e0d776c9d29ff9c33c82a40336d430e55
Jhonnatan Andre Zevallos <jzevallos@cdainfo.local>
2021-10-05 23:17:20 -0300

    first commit

---
+++ src/Services/Catalog/Catalog.Persistence.Database/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Catalog.Domain;
+using Catalog.Persistence.Database.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Persistence.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductInStock> Stocks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.HasDefaultSchema("catalog");
+            ModelConfig(builder);
+        }
+
+        private void ModelConfig(ModelBuilder modelBuilder)
+        {
+            new ProductConfiguration(modelBuilder.Entity<Product>());
+            new ProductInStockConfiguration(modelBuilder.Entity<ProductInStock>());
+        }
+    }
+}
