32fd3490b7239b4e16aefe586b64e65cf0d114b2
Jhonnatan Andre Zevallos <jzevallos@cdainfo.local>
2021-11-18 21:02:44 -0300

    cambios varios

--- src/Services/Catalog/Catalog.Api/Startup.cs
+++ src/Services/Catalog/Catalog.Api/Startup.cs
@@ -1,23 +1,17 @@
 using Catalog.Persistence.Database;
 using Catalog.Service.Queries;
-using Common.Logging;
 using HealthChecks.UI.Client;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 
 namespace Catalog.Api
 {
